9b3a3f2740844a8bbce19b4060dac02fed24e61f
Aafke Smal <aafke.smal@gmail.com>
2016-08-13 13:41:17 +0200

    create hotdogzz

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  // how to fetch tab url using activeInfo.tabid
+  chrome.tabs.get(activeInfo.tabId, function(tab){
+    chrome.tabs.executeScript({
+      "file": 'hotdog.js'
+    });
+  });
+});
